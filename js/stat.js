13bbce1726871a4dd02ec9a11ab7ba7d15928c8f
Polisonik <allred@inbox.ru>
2017-08-15 09:09:11 +0300

    Оформление кода в виде функций

--- js/stat.js
+++ js/stat.js
@@ -15,39 +15,46 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Ура вы победили!', 120, 30);
   ctx.fillText('Список результатов:', 120, 50);
 
-  // Находим наихудшее время
+  function getMaxItem(array) {
+    var maxItem = -1;
 
-  var maxTime = -1;
+    for (var i = 0; i < array.length; i++) {
+      var item = array[i];
 
-  for (var i = 0; i < times.length; i++) {
-    var time = times[i];
-    if (time > maxTime) {
-      maxTime = time;
+      if (item > maxItem) {
+        maxItem = item;
+      }
+    }
+    return maxItem;
+  }
+
+  function drawHistogram(arrayTimes, arrayNames, maxTime) {
+    var histogramHeight = 150;
+    var barWidth = 40;
+    var barIndent = 50;
+    var proportion = histogramHeight / (maxTime - 0);
+    var stepX = barWidth + barIndent;
+    var indentX = 140;
+    var indentY = 90;
+    var indentTop = 20;
+    var indentBottom = 10;
+    var opacity;
+
+    for (var i = 0; i < arrayTimes.length; i++) {
+
+      if (arrayNames[i] === 'Вы') {
+        ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+      } else {
+        opacity = Math.random();
+        ctx.fillStyle = 'rgba(000,000,255,' + opacity + ')';
+      }
+
+      ctx.fillRect(indentX + stepX * i, indentY + histogramHeight - arrayTimes[i] * proportion, barWidth, arrayTimes[i] * proportion);
+      ctx.fillStyle = '#000';
+      ctx.fillText(Math.round(arrayTimes[i]), indentX + stepX * i, indentY + histogramHeight - arrayTimes[i] * proportion - indentTop);
+      ctx.fillText(arrayNames[i], indentX + stepX * i, indentY + histogramHeight + indentBottom);
     }
   }
 
-  var histogramHeight = 150;
-  var barWidth = 40;
-  var barIndent = 50;
-  var proportion = histogramHeight / (maxTime - 0);
-  var stepX = barWidth + barIndent;
-  var indentX = 140;
-  var indentY = 90;
-  var indentTop = 20;
-  var indentBottom = 10;
-  var opacity;
-
-  for (i = 0; i < times.length; i++) {
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      opacity = Math.random();
-      ctx.fillStyle = 'rgba(000,000,255,' + opacity + ')';
-    }
-    ctx.fillRect(indentX + stepX * i, indentY + histogramHeight - times[i] * proportion, barWidth, times[i] * proportion);
-    ctx.fillStyle = '#000';
-    time = Math.round(times[i]);
-    ctx.fillText(time, indentX + stepX * i, indentY + histogramHeight - times[i] * proportion - indentTop);
-    ctx.fillText(names[i], indentX + stepX * i, indentY + histogramHeight + indentBottom);
-  }
+  drawHistogram(times, names, getMaxItem(times));
 };
