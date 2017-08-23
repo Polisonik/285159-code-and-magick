88f16653a065d9c37bdd156d3bb4b8f2514054df
Polisonik <allred@inbox.ru>
2017-08-23 20:47:14 +0300

    Исправление замечаний, реализация неповторения фамилий волшебников

--- js/setup.js
+++ js/setup.js
@@ -1,57 +1,49 @@
 'use strict';
-var wizardNames = getNames();
-var wizardSurnames = getSurnames();
-var coatColor = getCoatColor();
-var eyesColor = getEyesColor();
-var wizards = fillWizardsArray(wizardNames, wizardSurnames, coatColor, eyesColor);
 
-openWindowSetup();
-addContentWizardsList(wizards);
-openWindowWizardFriends();
+openSetup();
+addContentWizards();
+openSetupFriend();
 
 function getNames() {
   return ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 }
-
 function getSurnames() {
   return ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 }
-
 function getCoatColor() {
   return ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 }
-
 function getEyesColor() {
   return ['black', 'red', 'blue', 'yellow', 'green'];
 }
+// Создание массива из объектов, которые описывают друзей волшебника
+function generateWizardsArray() {
+  var wizardsOptions = [];
+  var numberWizards = 4;
+  var names = getNames();
+  var surnames = getSurnames();
+  var coatColors = getCoatColor();
+  var eyesColors = getEyesColor();
 
-function openWindowSetup() {
-  document.querySelector('.setup').classList.remove('hidden');
-}
+  for (var i = 0; i < numberWizards; i++) {
+    var indexFirst = getRandIndex(names);
+    var indexSecond = getRandIndex(surnames);
+    var indexThird = getRandIndex(coatColors);
+    var indexFourth = getRandIndex(eyesColors);
+    var choiceSurname = surnames.splice(indexSecond, 1);
 
-function openWindowWizardFriends() {
-  var userDialog = document.querySelector('.setup');
-  userDialog.querySelector('.setup-similar').classList.remove('hidden');
-}
-
-function getRandIndex(array) {
-  return Math.floor(Math.random() * array.length);
-}
-
-function fillWizardsArray(namesArray, surnamesArray, coatArray, eyesArray) {
-  var wizardsArray = [];
-  for (var j = 0; j < 4; j++) {
-    var randIndexNames = getRandIndex(namesArray);
-    var randIndexSurnames = getRandIndex(surnamesArray);
-    var randIndexCoat = getRandIndex(coatArray);
-    var randIndexEyes = getRandIndex(eyesArray);
-
-    wizardsArray.push({name: namesArray[randIndexNames] + ' ' + surnamesArray[randIndexSurnames], coatColor: coatArray[randIndexCoat], eyesColor: eyesArray[randIndexEyes]});
+    wizardsOptions.push({name: names[indexFirst] + ' ' + choiceSurname, coatColor: coatColors[indexThird], eyesColor: eyesColors[indexFourth]});
   }
-  return wizardsArray;
+  return wizardsOptions;
 }
 
-function drawWizard(wizard) {
+// Открытие окна с настройками игрока
+function openSetup() {
+  var userDialog = document.querySelector('.setup');
+  userDialog.classList.remove('hidden');
+}
+// Создание волшебника по шаблону
+function renderWizard(wizard) {
   var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
@@ -61,12 +53,24 @@ function drawWizard(wizard) {
 
   return wizardElement;
 }
-
-function addContentWizardsList(wizardsArray) {
+// Добавление новых DOM-элементов (волшебников) в список .setup-similar-list
+function addContentWizards() {
+  var arrayWizards = generateWizardsArray();
   var similarListElement = document.querySelector('.setup-similar-list');
+  var wizardsNumber = 4;
   var fragment = document.createDocumentFragment();
-  for (var i = 0; i < 4; i++) {
-    fragment.appendChild(drawWizard(wizardsArray[i]));
+
+  for (var i = 0; i < wizardsNumber; i++) {
+    fragment.appendChild(renderWizard(arrayWizards[i]));
   }
   similarListElement.appendChild(fragment);
+}
+// Открытие окна настоек друзей волшебника 
+function openSetupFriend() {
+  var userDialog = document.querySelector('.setup');
+  userDialog.querySelector('.setup-similar').classList.remove('hidden');
+}
+// Генерация случайного элемента массива
+function getRandIndex(array) {
+  return Math.floor(Math.random() * array.length);
 }
